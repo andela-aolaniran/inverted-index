ea63d90b6002aa53676d45bb47ae7bd2cc885fab
Olaniran Azeez <azeez.olaniran@andela.com>
2016-11-19 14:03:49 +0100

    test travis integration

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,38 +1,27 @@
-//const invertedIndex = new InvertedIndex();
-const invertedIndex = require(../../src/js/inverted-index);
-const bookFile = [
-  {
-    "title": "Alice in Wonderland",
-    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
-  },
-
-  {
-    "title": "The Lord of the Rings: The Fellowship of the Ring.",
-    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
-  }
-]
-
 
 describe("Read book Data:", function() {
   
 
   it("should read a json file and assert its not empty", function() {
-    expect(invertedIndex.readBookFile([])).toBe(false);
+    expect(true).toBe(false);
   });
 });
+/**
 
 describe("Populate Index:", function() {
   beforeEach(function() {
-    invertedIndex.readBookFile(bookFile);
+    //invertedIndex.readBookFile(bookFile);
+  }
   });
 
   it("index is created once JSON file has been read", function() {
-    expect(invertedIndex.indexMap).toBeDefined();
+    var a ;
+    expect(a).toBeDefined();
   });
 
   it("index should map string keys to the correct objects in the JSON array", function(){
-    let values = invertedIndex.mapIndex.get('Alice') ;
-    expect(values.has(1)).tobe(true);
+    //let values = invertedIndex.mapIndex.get('Alice') ;
+    expect(1>2).tobe(true);
   });
 
 });
@@ -40,8 +29,10 @@ describe("Populate Index:", function() {
 describe("Search index:", function() {
 
   it("searching the index returns an array of the indices of the correct objects that contain the words in the search query.", function() {
-    let result = invertedIndex.search('and');
-    expect(result.has(1)).toBe(true);
-    expect(result.has(2)).tobe(true);
+    //let result = invertedIndex.search('and');
+    expect(true).toBe(true);
+    expect(false).tobe(true);
   });
 });
+};
+*/
