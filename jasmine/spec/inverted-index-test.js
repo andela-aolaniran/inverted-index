7682adecb53c38f376fc1d3eeaff29de0213d3fb
Olaniran Azeez <azeez.olaniran@andela.com>
2016-12-08 06:20:59 +0100

    add spec for Searching Index with a word not in the created index

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -9,7 +9,6 @@ describe('Read book Data:', () => {
   it('should read a json file and assert that it is not empty', () => {
     expect(invertedIndex.readBook(empty_book)).toEqual(false);
     expect(invertedIndex.readBook()).toEqual(false);
-    expect(invertedIndex.readBook(empty_book)).toEqual(false);
   });
 });
 
@@ -29,14 +28,18 @@ describe('Populate Index', () => {
 });
 
 describe('Search Index', () => {
+  let searchResult = '';
   beforeEach(() => {
     invertedIndex.createIndex(fileName, books);
+    searchResult = invertedIndex
+      .searchIndex('alice in and elf man azeez', [fileName]);
   });
   it('searches the index and returns the right results', () => {
-    const searchResult = invertedIndex
-      .searchIndex('alice in and elf man azeez', [fileName]);
     expect(searchResult[fileName].alice).toEqual([0]);
     expect(searchResult[fileName].and).toEqual([0, 1]);
+  });
+  it(`searches the index and returns no results if the 
+    search term isn't in the index`, () => {
     expect(searchResult[fileName].azeez).toEqual([]);
   });
 });
